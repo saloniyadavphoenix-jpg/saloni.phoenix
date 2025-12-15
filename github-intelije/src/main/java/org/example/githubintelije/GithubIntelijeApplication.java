fa6da4bc37efaa2debb0b44f048cf49460ba663b
saloni.phoenix <saloniyadav.phoenix@gmail.com>
2025-12-15 21:52:51 +0530

    init commit

---
+++ github-intelije/src/main/java/org/example/githubintelije/GithubIntelijeApplication.java
@@ -0,0 +1,13 @@
+package org.example.githubintelije;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubIntelijeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GithubIntelijeApplication.class, args);
+    }
+
+}
---
+++ github-intelije/src/test/java/org/example/githubintelije/GithubIntelijeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.githubintelije;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubIntelijeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
